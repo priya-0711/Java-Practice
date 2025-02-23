f4d351a0a6ae8532327ca167c49b138325b5d782
Vinay Mirchandani <0105cs211134@oriental.ac.in>
2025-02-23 17:53:57 +0530

    exception handling

---
+++ src/com/practice/ExceptionHAndlingExample.java
@@ -0,0 +1,11 @@
+package com.practice;
+
+public class ExceptionHAndlingExample {
+    public static void main(String[] args)  {
+        //CTE: as Exception class is of checked Exception type
+        // and needs to be handled either using try-catch
+        // or using throws keyword as method signature
+            throw new Exception("");
+
+    }
+}
