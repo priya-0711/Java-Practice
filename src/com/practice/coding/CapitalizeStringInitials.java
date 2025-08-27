86ed7e7f92b5b2aeefa71106cfef3925854f15f6
priya-0711 <mirchandani1997@gmail.com>
2025-08-27 17:36:52 +0530

    Update CapitalizeStringInitials.java

--- src/com/practice/coding/CapitalizeStringInitials.java
+++ src/com/practice/coding/CapitalizeStringInitials.java
@@ -2,21 +2,17 @@ package org.example;
 
 public class CapitalizeStringInitials {
     public static void main(String[] args) {
-        String str="i love coding";
-        String[] s= str.split(" ");  //[i,love,coding]
-        String st=""; String string="";
-
-        for(int i=0;i<s.length;i++) {
-            st = s[i].toString(); //"love"
-            for (int j = 0; j < st.length(); j++) {
-                char c = st.charAt(0);
-                if (c == st.charAt(j)) {
-                    string =  string + " " + String.valueOf(c).toUpperCase();
-                } else {
-                    string = string +st.charAt(j);
-                }
-            }
-        }
-        System.out.println(string);
+         String str = "india loves coding"; // o/p: India Loves Coding
+        String[] st = str.split(" ");  //["india","loves","coding"]
+        StringBuilder sb = new StringBuilder();
+       for(String s: st) {  // s=india
+         if(!s.isEmpty())   {
+             sb.append(Character.toUpperCase(s.charAt(0)))
+                    .append(s.substring(1))
+                    .append(" ");
+         }
+       }
+        System.out.println(sb);
     }
 }
+
