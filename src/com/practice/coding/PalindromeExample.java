9084e58508dbb079cac6aa51aa2f8c89c4c5a0c9
priya-0711 <mirchandani1997@gmail.com>
2025-08-28 23:20:36 +0530

    Update PalindromeExample.java

--- src/com/practice/coding/PalindromeExample.java
+++ src/com/practice/coding/PalindromeExample.java
@@ -12,6 +12,16 @@ public class PalindromeExample {
             System.out.println("true");
         else
             System.out.println("false");
-
+ }   
+    //   #approach 2:
+public static boolean palindrome(String str) {
+        for(int i=0; i< str.length()/2;i++) {
+            if(str.charAt(i)!=str.charAt(str.length()-i-1)){
+                return false;
+            }
+        }
+                return true;
     }
+
+    
 }
